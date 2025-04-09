135d1e045acdb9c5f76c7d1ca026efd50c9554b7
Ashley Newton <ashleynewton@Ashleys-MacBook-Air.local>
2025-04-09 14:31:55 -0400

    Improve tag styling

--- client/app/components/Tag.tsx
+++ client/app/components/Tag.tsx
@@ -13,6 +13,16 @@ export function Tag(props: ITagProps): JSX.Element {
     <Chip
       label={title}
       className="bg-gradient-to-r from-blue-200 to-cyan-200 text-center rounded-full mt-1 mb-1"
+      sx={{
+        fontSize: '1rem',
+        padding: '0.2rem 0.5rem',
+        maxWidth: '12rem',
+        height: 'auto',
+        '& .MuiChip-label': {
+          display: 'block',
+          whiteSpace: 'normal',
+        },
+      }}
     />
   )
 }
