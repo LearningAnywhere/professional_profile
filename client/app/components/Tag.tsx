3b90d7eafad824c8bdcca8772c886eb39a92826f
Ashley Newton <ashleynewton@Ashleys-MacBook-Air.local>
2025-04-09 14:06:28 -0400

    Make tag a wrapper

--- client/app/components/Tag.tsx
+++ client/app/components/Tag.tsx
@@ -1,4 +1,6 @@
 import type { JSX } from 'react'
+import * as React from 'react'
+import Chip from '@mui/material/Chip'
 
 interface ITagProps {
   title: string
@@ -8,8 +10,9 @@ export function Tag(props: ITagProps): JSX.Element {
   const { title } = props
   // TODO: Add some color variants
   return (
-    <div className="bg-gradient-to-r from-blue-200 to-cyan-200 text-center rounded-full mt-1 mb-1">
-      {title}
-    </div>
+    <Chip
+      label={title}
+      className="bg-gradient-to-r from-blue-200 to-cyan-200 text-center rounded-full mt-1 mb-1"
+    />
   )
 }
